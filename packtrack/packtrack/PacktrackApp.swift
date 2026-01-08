8d6956b3b4306ee00f1dac4faf0c3f95c9b8f4c3
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2026-01-08 11:43:19 -0600

    Add .gitignore for Xcode/Swift projects

---
+++ packtrack/packtrack/PacktrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  packtrackApp.swift
+//  packtrack
+//
+//  Created by Nathanial Henniges on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct packtrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
